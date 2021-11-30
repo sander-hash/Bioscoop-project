a91f3369a8e4b53fdfac2b186eb7f8a6c2fb412a
Sander Jongenelen <sanderjongenelen@MacBook-Pro-van-Sander.local>
2021-11-30 22:07:39 +0100

    Film titel filmpagina aangepast

--- functions.php
+++ functions.php
@@ -172,7 +172,7 @@ return $data;
         <div class="bg-gray-100">
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="max-w-2xl mx-auto py-16 sm:py-24 lg:py-16 lg:max-w-none">
-      <h2 class="text-2xl font-extrabold text-gray-900">Films</h2>
+      <h2 class="text-2xl font-extrabold text-gray-900">'.$row["film"].'</h2>
 
       <div class="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
         <div class="group relative">
