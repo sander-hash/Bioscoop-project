3a6a801d60414059719dbc251dcf64f4f801cf72
sjoerdverhelst <sjoerd42471@live.nl>
2021-12-03 15:46:06 +0100

    testing

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$user="root";
-$pass="";
+$user="sjoerd";
+$pass="MyNewPass4!";
 try {
-    $db = new PDO('mysql:host=localhost;dbname=bioscoop', $user, $pass);
+    $db = new PDO('mysql:host=db;dbname=bioscoop', $user, $pass);
 } catch (PDOException $e) {
     error_log($e->getMessage(), 3, "error_log.txt");
     die('Er is een probleem met de database verbinding');
