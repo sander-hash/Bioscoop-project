afee532346e87c45e61ff60bdac7c6cfb95f2fb8
Sander Jongenelen <sanderjongenelen@MacBook-Pro-van-Sander.local>
2021-12-03 13:26:37 +0100

    Loginscherm veranderd

--- login.php
+++ login.php
@@ -61,11 +61,7 @@ include 'functions.php'
 
 
 
-              <div class="text-sm">
-                <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-                  Forgot your password?
-                </a>
-              </div>
+
             </div>
 
             <div>
