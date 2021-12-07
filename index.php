4ca1cc200d5d4cdc3d9c9cbcfad03067300cdbb5
sander-hash <sanderjongenelen@hotmail.nl>
2021-12-07 14:02:57 +0100

    Vraagteken weggehaald

--- index.php
+++ index.php
@@ -81,7 +81,7 @@
         <section class="text-gray-600 body-font">
             <div class="container px-5 py-24 mx-auto">
                 <div class="text-center mb-20">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Hoe up-to-date zijn wij??</h1>
+                <h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900 mb-4">Hoe up-to-date zijn wij?</h1>
                 <div class="flex mt-6 justify-center">
                     <div class="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
                 </div>
